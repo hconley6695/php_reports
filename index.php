e28b18fe6c3c17acabb47ea866539b513446db41
Heather Conley <hconley6695@gmail.com>
2017-01-27 08:02:07 -0500

    having to sort data in the beginning, changing it will change the table

--- index.php
+++ index.php
@@ -32,86 +32,165 @@
 	<?php
 
 		echo '<div class="container"><table border=1>';
-		$fileName = 'Module_2_Orders.csv';
+		$fileName = 'module_2_orders.csv';
 
-		$handle = fopen($fileName, 'r');
-		$data_in = fread($handle, filesize($fileName));
-		// echo $data_in;
-		fclose($handle);
+		$row = 1;
+		// $col = 0;
+		$titles = array();
+		if (($handle = fopen($fileName, "r")) !== FALSE) {
+		  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 
-		$data_array = array();
-		$data_array = preg_split("/\r\n|\n|\r/", $data_in);//USE THE CELL/ROW DELIMITER; THIS TARGETS END OF LINE
-		
-		for($i = 0; $i < count($data_array); $i++) {
-			echo '<tr>';
-			$rows = array();
-			$rows = explode(',', $data_array[$i]);// use the cell/row delimiter what u need!
+		  	if ($row == 1){
+		  		$titles = $data;
+		  		// print_r($titles[0]);//THIS JUST EQUALS 'ORDER ID'
+		  		$num= count($data);
+		  		$row++;
+		  		// for ($c=0; $c < $num; $c++) {
+
+		  		// 	// print_r($data[$c]);
+		  		// 	$new = array($data[$c]);
+		  		// 	echo'<pre>';
+		  		// 	print_r($new);
+		  		// 	// $stuff = $data[$c];
+		  		// 	return $new;
+		  		// }
+		  	} else {
+
+			  	$num = count($data);
+			    echo "<p> $num fields/arrays in line $row: <br /></p>\n";
+			    $row++;
+			    // echo '<pre>';
+		    	// print_r($data);
+
+			    for($c=0; $c < $num; $c++) {
+				    $new[$titles[$c]] = $data[$c];				    	
+				    echo '<pre>';
+				    print_r($new['Name']);
+
+		    	}
+		  	}
+	      }
+		}
+
+		// echo '<pre>';
+		// print_r($titles);
 
 
+		// DAY 4 OF DRUDGING THROUGH THIS:
+
+//FIRST ATTEMPT		
+		// function importCsvToArray($fileName) {
+		// 	$row = 0;
+		// 	$col = 0;
+
+		// 	if(($handle = fopen($fileName, "r")) !== FALSE) {
+		// 		while(($row = fgetcsv($handle)) !== FALSE) {
+		// 			echo "new row";
+		// 			// echo $row;
+		// 			echo '<pre>';
+		// 			print_r($row);
+		// 			echo '</pre>';
+		// 			// if(empty($fields)) {
+		// 			// 	$fields = $row;
+		// 			// 	continue;
+		// 			// }
+		// 			// foreach($row as $k => $value) {
+		// 			// 	$results[$col][$fields[$k]] = $value; 
+		// 			// }
+		// 			// $col++;
+		// 			// unset($row);
+		// 		}
+		// 		// if(!feof($handle)){
+		// 		// 	echo "Error: unexpected fgets() failing.";
+		// 		// }
+		// 		fclose($handle);
+		// 	}
+		// 	// return $results;
+		// // }
+
+		// foreach ($csv_file as $row => $value) {
+		// 	echo $row[$col];
+
+		// }
+
+
+////////////////
 
 
 		// THIS SPLITS THE NAMES INTO FIRST AND LAST
-			$array_names = explode(" ", $rows[1], 2);
-			$firstName = $array_names[0];
-			$lastName = $array_names[1];
+			// $array_names = explode(" ", $rows[1], 2);
+			// $firstName = $array_names[0];
+			// $lastName = $array_names[1];
 
 		// TRYING TO ADD AN EMPTY CELL TO ROWS WITH EMPTY LAST NAME.  RIGHT NOW, AN EMAIL IS FILLING IN THAT SPOT/INDEX.
-			if($rows[1] != "Name") {
+			// if($rows[1] != "Name") {
+			// 	$rows[1] = $firstName;
+			// 	array_splice($rows, 2, 0, $lastName);
+			// } else {
+			// 	$rows[1] = "First Name";
+			// 	array_splice($rows, 2, 0, "Last Name");
+			// } 
 
-				// $mystr = $rows[1];
-				// $findme = "@";
-				// $incl = strpos($mystr, $findme); 
+//FIRST ATTEMPT AT SORTING
+			// function sortByLastName($arr, $col, $dir=SORT_ASC) {
+			// 	$sort_last = array();
+			// 	foreach ($$arr as $key => $row) {
+			// 		$sort_last[$key] = $row[$col];
+					
+			// 	}
+			// 	array_multisort($sort_last, $dir, $arr);
+			// }
+			// sortByLastName($rows);
 
-				// if (!$incl) {
-				//THIS FIRST IF STATEMENT IS NOT MATCHING.
-				// if (preg_match("@", $lastName)) {
-					$rows[1] = $firstName;
-					array_splice($rows, 2, 0, $lastName);
-				// } else {
-					// $rows[1] = $firstName;
-					// $lastName = "empty";
-					// array_splice($rows, 2, 0, $lastName);				
+// SECOND ATTEMPT AT SORTING
+			// function sortIt($outerArr, $innerArr, $direction) {
+			// 	$rowsB = array();
+			// 	foreach($outerArr as $innerArr => $stuff) {
+			// 		// print_r($innerArr);//ANSWER: 0123456
+			// 		// echo $stuff;//ANSWER: ORDER IDFIRST NAMELAST NAME....
+			// 		// foreach($innerArr as $inmostArr => $stuff) {
+			// 		// 	print_r($inmostArr);
+			// 		// }
 
-				// }
+			// 		// print_r($rowsB);
+			// 		}
+			// 		// array_multisort($rowsB);
+			// }
+			// sortIt($rows, SORT_ASC);
 
-			} else {
-				$rows[1] = "First Name";
-				array_splice($rows, 2, 0, "Last Name");
 
-			} 
 
-			//THIS IS WHERE $ROWS ARRAY HAS THE VALUES THAT I CAN SORT THEM.
 
-			
-			echo $rows[2];//THIS IS LAST NAME.
+
+
+		// THIS WAS WORKING!!!!!!!!!!!!!!!!
+		// for($i = 0; $i < count($data_array); $i++) {
+		// 	echo '<tr>';
+		// 	$rows = array();
+		// 	$rows = explode(',', $data_array[$i]);// use the cell/row delimiter what u need!
+		// 	print_r($rows);//THIS GIVES ME AN ARRAY WITH ARRAYS INSIDE EACH THAT HAVE ARRAYS INSIDE EACH WITH ONE WORD??
 
 
 			// THIS FOR LOOP CREATES THE TABLE
-			for($cell = 0; $cell < count($rows); $cell++) {
+			// for($cell = 0; $cell < count($rows); $cell++) {
 
-				$bgcolor='#ccc';
-				if ($rows[0] != 0) {
-					// THIS IF/ELSE STATEMENT CREATES RED ROWS FOR THOSE THAT AREN'T 
-					if ($rows[7] != "$0.00") {
-						$bgcolor='red';
-						echo '<td style="background-color:' . $bgcolor . '">' . $rows[$cell] . '</th\d>';
-					} else {
-						$bgcolor='#ccc';
-						echo '<td style="background-color:' . $bgcolor . '">' . $rows[$cell] . '</td>';
-					}
-				} else {
-
-					echo '<th style="background-color:' . $bgcolor . '">' . $rows[$cell] . '</th>';
-				}
-			}
-
-			echo '</tr>';	
-		}
-
+			// 	$bgcolor='#ccc';
+			// 	if ($rows[0] != 0) {
+					// THIS IF/ELSE STATEMENT CREATES RED ROWS FOR THOSE THAT HAVE A BALANCE 
+			// 		if ($rows[7] != "$0.00") {
+			// 			$bgcolor='red';
+			// 			echo '<td style="background-color:' . $bgcolor . '">' . $rows[$cell] . '</th\d>';
+			// 		} else {
+			// 			$bgcolor='#ccc';
+			// 			echo '<td style="background-color:' . $bgcolor . '">' . $rows[$cell] . '</td>';
+			// 		}
+			// 	} else {
+			// 		echo '<th style="background-color:' . $bgcolor . '">' . $rows[$cell] . '</th>';
+			// 	}
+			// }
+			// echo '</tr>';	
+		// }
 		echo '</table></div>';
-
-
-
 
 	?>
 
